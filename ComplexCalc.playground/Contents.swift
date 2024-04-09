4eb5b91790c2f90cde39ed79c390b846c5942927
Aina Engelbrekt <ainae@uw.edu>
2024-04-08 22:58:35 -0700

    done

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -38,9 +38,34 @@ class Calculator {
         return numbers.reduce(0, +)
     }
     
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+            return (lhs.0 + rhs.0, lhs.1 + rhs.1)
+        }
+    
+    func add(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var total: [String: Int] = [:]
+        
+        for key in lhs.keys {
+            total[key] = (lhs[key] ?? 0) + (rhs[key] ?? 0)
+        }
+        return total
+    }
+    
     //subtraction functions
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
+    }
+    
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (lhs.0 - rhs.0, lhs.1 - rhs.1)
+    }
+    
+    func subtract(lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
+        var total: [String: Int] = [:]
+        for key in lhs.keys {
+            total[key] = (lhs[key] ?? 0) - (rhs[key] ?? 0)
+        }
+        return total
     }
     
     //multiply functions
@@ -58,16 +83,21 @@ class Calculator {
         return lhs / rhs
     }
     
-    //mathOp
+    //mathOp functions
     func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
             return op(lhs, rhs)
         }
+    
+    func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
+        return args.reduce(beg, op)
+    }
     
     //count function
     func count(_ numbers: [Int]) -> Int {
          return numbers.count
      }
     
+    //average functions
     func avg(_ numbers: [Int]) -> Double {
         guard !numbers.isEmpty else { return 0 }
         let total = numbers.reduce(0, +)
